bbfe107ea015b0f27911347e975323400caa7609
fsavas <savasusame@gmail.com>
2022-02-22 15:26:57 +0300

    fix: node env issues

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -1,7 +1,16 @@
 import { MongoClient } from "mongodb";
 import secrets from "../secrets";
 
-const uri = `mongodb+srv://appicient:${secrets.Mongodb.password}@main.fzy7k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const mongoUser =
+  process.env.NODE_ENV === "development"
+    ? secrets.Mongodb.user
+    : process.env.MONGODB_USER;
+const mongoPasswd =
+  process.env.NODE_ENV === "development"
+    ? secrets.Mongodb.password
+    : process.env.MONGODB_PASSWD;
+
+const uri = `mongodb+srv://${mongoUser}:${mongoPasswd}@main.fzy7k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
